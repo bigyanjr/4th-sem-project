64504570caf9220eb0fb87a8cf63a6e7781e5cce
bigyan rijal <rijalbigyan1@gmail.com>
2025-11-22 15:45:32 +0545

    Initial commit of Laravel 5.8 project

---
+++ app/User.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App;
+
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+
+class User extends Authenticatable
+{
+    use Notifiable;
+
+    protected $fillable = [
+        'f_name',
+        'l_name',
+        'email',
+        'image',
+        'password',
+    ];
+
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
+
+    public function getFullNameAttribute() {
+        return ucfirst($this->f_name) . ' ' . ucfirst($this->l_name);
+    }
+}
